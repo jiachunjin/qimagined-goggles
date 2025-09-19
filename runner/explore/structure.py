71b61be8e4efd14e4d7f62f189df16536e6ee8ce
Jiachun Jin <jiachun22.jin@gmail.com>
2025-09-19 15:05:29 +0800

    sync

--- runner/explore/structure.py
+++ runner/explore/structure.py
@@ -2,7 +2,7 @@ import torch
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer
 
 
-def text_encode():
+def text_encode_bsz_1():
     device = torch.device("cuda:0")
     dtype = torch.bfloat16
     # ---------- load text encoder ----------
@@ -20,11 +20,22 @@ def text_encode():
         formatted_prompt, max_length=512, padding=True, truncation=True, return_tensors="pt"
     ).to(device)
 
-    print(txt_tokens.input_ids[0].shape, tokenizer.decode(txt_tokens.input_ids[0]))
-    print(txt_tokens.attention_mask[0].shape)
+    # print(txt_tokens.input_ids[0].shape, tokenizer.decode(txt_tokens.input_ids[0]))
+    # print(txt_tokens.attention_mask[0].shape)
 
+    encoder_hidden_states = text_encoder(
+        input_ids            = txt_tokens.input_ids,
+        attention_mask       = txt_tokens.attention_mask,
+        output_hidden_states = True,
+    )
+    hidden_states = encoder_hidden_states.hidden_states[-1]
+    print(hidden_states.shape)
+
+    chosen_hidden_states = [e[drop_idx:] for e in hidden_states]
+    print(chosen_hidden_states.shape)
+    
 
     
 if __name__ == "__main__":
-    text_encode()
+    text_encode_bsz_1()
     
