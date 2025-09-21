02f2df2a7126c7566ce22750b58ef420bb787aad
Jiachun Jin <jiachun22.jin@gmail.com>
2025-09-21 16:25:32 +0800

    sync

--- runner/explore/edit.py
+++ runner/explore/edit.py
@@ -1,3 +1,6 @@
+import torch
+import os
+from PIL import Image
 from diffusers import QwenImageEditPipeline
 
 @torch.no_grad()
@@ -5,21 +8,34 @@ def edit_with_pipeline():
     pipeline = QwenImageEditPipeline.from_pretrained("/data/phd/jinjiachun/ckpt/Qwen/Qwen-Image-Edit")
     print("pipeline loaded")
     pipeline.to(torch.bfloat16)
-    pipeline.to("cuda:1")
+    pipeline.to("cuda:0")
     pipeline.set_progress_bar_config(disable=None)
-    image = Image.open("assets/letter1.webp").convert("RGB")
-    prompt = '把信件上的中文去除掉，改写成: "Autumn Breeze, Letter Arrives"'
-    inputs = {
-        "image": image,
-        "prompt": prompt,
-        "generator": torch.manual_seed(0),
-        "true_cfg_scale": 4.0,
-        "negative_prompt": " ",
-        "num_inference_steps": 50,
-    }
+    while True:
+        # 等待用户输入
+        prompt = input("请输入prompt: ")
+        img_path = input("img path:")
+        try:
+            image = Image.open(img_path).convert("RGB")
+        except Exception:
+            print("加载图片出错，重新加载")
+            continue
+        if prompt == "exit":
+            break
 
-    with torch.inference_mode():
-        output = pipeline(**inputs)
-        output_image = output.images[0]
-        output_image.save("output_image_edit.png")
-        print("image saved at", os.path.abspath("output_image_edit.png"))
+        inputs = {
+            "image": image,
+            "prompt": prompt,
+            "generator": torch.manual_seed(0),
+            "true_cfg_scale": 4.0,
+            "negative_prompt": " ",
+            "num_inference_steps": 50,
+        }
+
+        with torch.inference_mode():
+            output = pipeline(**inputs)
+            output_image = output.images[0]
+            output_image.save("output_image_edit.png")
+            print("image saved at", os.path.abspath("output_image_edit.png"))
+
+if __name__ == "__main__":
+    edit_with_pipeline()
