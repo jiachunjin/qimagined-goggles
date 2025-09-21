21557ef1f929f483027d62ebb5afd449de40d352
Jiachun Jin <jiachun22.jin@gmail.com>
2025-09-21 17:26:21 +0800

    sync

--- runner/explore/edit.py
+++ runner/explore/edit.py
@@ -42,13 +42,16 @@ def edit_with_pipeline():
 def edit_with_thinking():
     pipeline = QwenImageEditPipeline.from_pretrained("/data/phd/jinjiachun/ckpt/Qwen/Qwen-Image-Edit")
     print("pipeline loaded")
+    device = "cuda:0"
+    dtype = torch.bfloat16
     pipeline.to(torch.bfloat16)
     pipeline.to("cuda:0")
     pipeline.set_progress_bar_config(disable=None)
 
     while True:
-        prompt = input("请输入prompt: ")
-        img_path = input("img path:")
+        prompt = ["把白板上的字翻译成英文"]
+        # img_path = input("img path:")
+        img_path = "/data/phd/jinjiachun/codebase/qimagined-goggles/asset/whiteboard.png"
         try:
             image_raw = Image.open(img_path).convert("RGB")
         except Exception:
@@ -75,60 +78,61 @@ def edit_with_thinking():
             return_tensors="pt",
         ).to(device)
 
-    # ----- continue the generation -----
-    generation_output = pipeline.text_encoder.generate(
-        **model_inputs,
-        max_new_tokens=512,
-        output_hidden_states=True,
-        return_dict_in_generate=True,
-        output_scores=True
-    )
-    generated_ids = generation_output.sequences
-    all_hidden_states = generation_output.hidden_states
-    last_layer_hidden_states = []
-    for token_hidden_states in all_hidden_states:
-        last_layer_hidden_states.append(token_hidden_states[-1])
-    final_hidden_states = torch.cat(last_layer_hidden_states, dim=1)
+        # ----- continue the generation -----
+        generation_output = pipeline.text_encoder.generate(
+            **model_inputs,
+            max_new_tokens=512,
+            output_hidden_states=True,
+            return_dict_in_generate=True,
+            output_scores=True,
+            do_sample=True,
+        )
+        generated_ids = generation_output.sequences
+        all_hidden_states = generation_output.hidden_states
+        last_layer_hidden_states = []
+        for token_hidden_states in all_hidden_states:
+            last_layer_hidden_states.append(token_hidden_states[-1])
+        final_hidden_states = torch.cat(last_layer_hidden_states, dim=1)
 
-    print(final_hidden_states.shape)
+        print(final_hidden_states.shape)
 
-    # ----- print一下qwenvl生成了些什么东西 -----
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-    output_text = pipeline.processor.batch_decode(
-        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-    )
-    print("生成的文本:", output_text)
+        # ----- print一下qwenvl生成了些什么东西 -----
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
+        ]
+        output_text = pipeline.processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+        print("生成的文本:", output_text)
 
-    # ----- 进一步处理hidden states得到续写的东西 -----
-    split_hidden_states = [e[drop_idx:] for e in final_hidden_states]
-    attn_mask_list = [torch.ones(e.size(0), dtype=torch.long, device=e.device) for e in split_hidden_states]
-    max_seq_len = max([e.size(0) for e in split_hidden_states])
-    prompt_embeds = torch.stack(
-        [torch.cat([u, u.new_zeros(max_seq_len - u.size(0), u.size(1))]) for u in split_hidden_states]
-    )
-    encoder_attention_mask = torch.stack(
-        [torch.cat([u, u.new_zeros(max_seq_len - u.size(0))]) for u in attn_mask_list]
-    )
-    prompt_embeds = prompt_embeds.to(dtype=dtype, device=device)
-    print(prompt_embeds.shape)
-    print(encoder_attention_mask.shape)
+        # ----- 进一步处理hidden states得到续写的东西 -----
+        split_hidden_states = [e[drop_idx:] for e in final_hidden_states]
+        attn_mask_list = [torch.ones(e.size(0), dtype=torch.long, device=e.device) for e in split_hidden_states]
+        max_seq_len = max([e.size(0) for e in split_hidden_states])
+        prompt_embeds = torch.stack(
+            [torch.cat([u, u.new_zeros(max_seq_len - u.size(0), u.size(1))]) for u in split_hidden_states]
+        )
+        encoder_attention_mask = torch.stack(
+            [torch.cat([u, u.new_zeros(max_seq_len - u.size(0))]) for u in attn_mask_list]
+        )
+        prompt_embeds = prompt_embeds.to(dtype=dtype, device=device)
+        print(prompt_embeds.shape)
+        print(encoder_attention_mask.shape)
 
-    inputs = {
-        "image": image_raw,
-        "prompt_embeds": prompt_embeds,
-        "prompt_embeds_mask": encoder_attention_mask,
-        "generator": torch.manual_seed(0),
-        "true_cfg_scale": 4.0,
-        "negative_prompt": " ",
-        "num_inference_steps": 50,
-    }
+        inputs = {
+            "image": image_raw,
+            "prompt_embeds": prompt_embeds,
+            "prompt_embeds_mask": encoder_attention_mask,
+            "generator": torch.manual_seed(0),
+            "true_cfg_scale": 4.0,
+            "negative_prompt": " ",
+            "num_inference_steps": 50,
+        }
 
-    with torch.inference_mode():
-        output = pipeline(**inputs)
-        output_image = output.images[0]
-        output_image.save(f"edit_{prompt}.png")
+        with torch.inference_mode():
+            output = pipeline(**inputs)
+            output_image = output.images[0]
+            output_image.save(f"edit_{prompt}.png")
 
 if __name__ == "__main__":
     edit_with_thinking()
